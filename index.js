c7bfb45c62b97125a07b903d83f4cf6adfceb82a
LingZhouChen <lzcciao@gmail.com>
2023-09-11 12:14:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// multiply nums to equal to 62
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+// generate random int > 0
+let random = Math.floor(Math.random() * 10) + 1;
+
+// calculate remainder = 4
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+
+// return 20 as highest number in set
+let numList = (1, 2, 3, 4, 20);
+const max = Math.max(numList);
+
+// alternative
+// let numList = [1, 2, 3, 4, 20];
+// const max = Math.max(...numList);
